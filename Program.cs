d0f4bf859c0404b80cc8a1593129355ad9086797
walumo <miikka.hakulinen@awacademy.training>
2022-06-15 09:45:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2DelegatesEx
+{
+    class Program
+    {
+        public delegate int Del(int num);
+        public static int TimesTen(int num)
+        {
+            return num * 10;
+        }
+        public static int TimesHundred(int num)
+        {
+            return num * 100;
+        }
+        public static int TimesThousand(int num)
+        {
+            return num * 1000;
+        }
+        static void Main(string[] args)
+        {
+            int number = 458;
+            Del loader1 = new Del(TimesTen);
+            Del loader2 = new Del(TimesHundred);
+            Del loader3 = new Del(TimesThousand);
+
+            Console.WriteLine(loader1(number));
+            Console.WriteLine(loader2(number));
+            Console.WriteLine(loader3(number));
+        }
+    }
+}
